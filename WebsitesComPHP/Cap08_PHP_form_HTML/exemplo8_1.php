a15fdd43e2b034e82e3b4ec372b7b71655e88e9d
Pedro Arns Junior <pedroarnsjr@outlook.com>
2022-10-21 11:35:53 -0300

    Capitulo 8 PHP form HTML

--- WebsitesComPHP/Cap08_PHP_form_HTML/exemplo8_1.php
+++ WebsitesComPHP/Cap08_PHP_form_HTML/exemplo8_1.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exemplo8_1</title>
+</head>
+<body>
+   
 <?php
 
     $texto = "<img src=http://siteporno.com.br/foto.jpg>";
@@ -6,3 +16,7 @@
     echo $novo_texto;
 
 ?>
+
+</body>
+</html>
+
--- WebsitesComPHP/Cap08_PHP_form_HTML/recebe_dados.php
+++ WebsitesComPHP/Cap08_PHP_form_HTML/recebe_dados.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>recebe_dados                                                               </title>
 </head>
 <body>
     <?php
@@ -17,21 +17,21 @@
         $estado = $_POST["estado"];
         $erro=0;
         if(strlen($username)<5)
-        { echo "O username deve possuir no mínimo 5 caracteres.<br>"; $erro=1; }
+        { echo "<h3>O username deve possuir no mínimo 5 caracteres.<br></h3>"; $erro=1; }
         if(strlen($senha)<5)
-        { echo "A senha deve possuir no mínimo 5 caracteres.<br>"; $erro=1; }
+        { echo "<h3>A senha deve possuir no mínimo 5 caracteres.<br></h3>"; $erro=1; }
         if($username == $senha)
-        { echo "O username e a senha devem ser diferentes.<br>"; $erro=1; }
+        { echo "<h3>O username e a senha devem ser diferentes.<br></h3>"; $erro=1; }
         if(empty($nome) OR strstr($nome, ' ')==FALSE)
-        { echo "Favor digite seu nome corretamente.<br>"; $erro=1; }
+        { echo "<h3>Favor digite seu nome corretamente.<br></h3>"; $erro=1; }
         if(strlen($email)<8 || strstr($email, '@')==FALSE)
-        { echo "Favor digitar seu email corretamente.<br>"; $erro=1;}
+        { echo "<h3>Favor digitar seu email corretamente.<br></h3>"; $erro=1;}
         if(empty($cidade))
-        { echo "Favor digitar sua cidade.<br>"; $erro=1; }
+        { echo "<h3>Favor digitar sua cidade.<br></h3>"; $erro=1; }
         if(strlen($estado)!=2)
-        { echo "Favor digite seu estado corretamente.<br>"; $erro=1; }
+        { echo "<h3>Favor digite seu estado corretamente.<br></h3>"; $erro=1; }
         if($erro==0)
-        { echo "Todos os dados foram digitados corretamente!"; }
+        { echo "<h3>Todos os dados foram digitados corretamente!</h3>"; }
         
 
     ?>
