4bbece63474515dcef00daf3bcdf508dcbdc870e
Pedro Arns Junior <pedroarnsjr@outlook.com>
2022-10-21 11:44:22 -0300

    Capitulo 9 Informacoes Por Paginas

--- WebsitesComPHP/Capitulo09_Pas_InfoPorPaginas/gerencia.php
+++ WebsitesComPHP/Capitulo09_Pas_InfoPorPaginas/gerencia.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>gerencia</title>
+</head>
+<body>
+    
 <?php 
 
     $operacao = $_POST["operacao"];
@@ -16,6 +26,11 @@
         //realiza a exclusão do produto
         echo "Produto excluido";
     }
-
-
 ?>
+
+
+</body>
+</html>
+
+
+
