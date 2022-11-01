82d8e374d395a276174891b5ce19f63e4378aa30
Pedro Arns Junior <pedroarnsjr@outlook.com>
2022-11-01 13:57:41 -0300

    Capitulo 12 PHP com banco de dados parte 2

--- WebsitesComPHP/Cap12_PHP_com_BancoDeDados/livro_visitas.php
+++ WebsitesComPHP/Cap12_PHP_com_BancoDeDados/livro_visitas.php
@@ -1,14 +1,40 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <body>
-    <h1>Livro de visitas</h1>
-    <?php
-    $incluir = 0;
-    if(isset($_POST["nome"]) && isset($_POST["cidade"])
-       && isset($_POST(["estado"]) && isset($_POST["mensagem"]))
+  <h1>Livro de visitas</h1>
+  <?php
+   $incluir = 0;
+   if(isset($_POST["nome"]) && isset($_POST["cidade"]) 
+      && isset($_POST(["estado"]) && isset($_POST["mensagem"]))
+   {
+      $nome = $_POST["nome"];
+      $cidade = $_POST["cidade"];
+      $estado = $_POST["estado"];
+      $mensagem = $_POST["mensagem"];
+      $incluir = 1;
+    }
+    
+    if($db = sqlite_open('bdteste', 0666, $erro))
     {
-        $nome = $_POST["nome"];
-    }   
+        //grava a mensagem recebida
+        if($incluir)
+           sqlite_query($db, "INSERT INTO mensagens 
+              VALUES ('$nome', '$cidade', '$estado', '$mensagem')");
+        // exibe todas as mensagens
+        $res =  sqlite_query($db, 'SELECT * ROM mensagem');
+        $linhas = sqlite_num_rows($res);
+        for($i=0; $i<$linhas; $i++)
+        {
+            $linhas = sqlite_num_rows($res);
+            echo "<b>Nome</b>:".$linha["nome"]. "<br>";
+            echo "<b>Cidade</b>:".$linha["cidade"]. "<br>";
+            echo "<b>Estado</b>:".$linha["estado"]. "<br>";
+            echo "<b>Mensagem</b>:".$linha["nmensagem"]. "<br>";
+        }
+        sqlite_close($db);       
+    }
+    else
+        die($erro);   
 
     ?>
 </body>
