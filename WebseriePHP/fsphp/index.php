02554de808f57e91e3bbd5944c623c11653c680a
Pedro Arns Junior <pedroarnsjr@outlook.com>
2022-11-02 16:33:02 -0300

    WebseriePHP - fsphp-inciando um projeto

---
+++ WebseriePHP/fsphp/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FSPHP: Iniciando Um Projeto</title>
+    <link rel="stylesheet" href="assets/style.css"/>
+</head>
+<body>
+ <?php
+   /**
+    * Bloco PHP com variavel start
+    * e comando de saida
+    */
+    $start = "vamos começar";
+    echo "<h1>Olá Mundo! ($start)</h1>";
+    echo "<p id='js'>Loading...</p>";
+ ?>
+
+ <script src="assets/scripts.js"></script>
+</body>
+</html>
---
+++ WebseriePHP/leiame.md
@@ -0,0 +1,2 @@
+Webserie PHP do Jeito Certo 100% gratuita com 06 episodios
+Aula 02 do episodio 1 - 01.06 - iniciando um projeto - aprendendo boas praticas do PHP  
