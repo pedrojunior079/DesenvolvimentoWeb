a827e35e892f6e530983363a72173a4d743a18df
Pedro Arns Junior <pedroarnsjr@outlook.com>
2022-11-01 23:04:46 -0300

    Cap13 - logout.php, pagina_inicial.php e valida_cookies.inc

---
+++ WebsitesComPHP/Cap13_Cookies_Sessoes/pagina_inicial.php
@@ -0,0 +1,11 @@
+<?php
+include "valida_cookies.inc";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<body>
+   Seja bem vindo ao meu site aqui neste espaço.<br>
+   Coloque o conteudo do seu site aqui neste espaço.<br>
+   <p><a href="logout.php">LOGOUT</a></p> 
+</body>
+</html>
