8b1d5c06e8c6201972b19988da985dfd5cb0b672
Pedro Arns Junior <pedroarnsjr@outlook.com>
2024-08-30 23:19:40 -0300

    Update

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
  Repositorio de Cursos de Websites de PHP, HTML, CSS e Javascript 
 
  Livro Desenvolvendo Website com PHP da Editora Novatec Ano de 2008 da 5ª edição 
+
+ Usando versão do PHP 8.2.12 
--- TutorialPHP/Tipos/typesString5.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $string = 'string';
-  echo "O caractere no indice -2 é $string[-2].", PH_EOL;
-  $string[-3] = 'o';
-  echo "Alterar o caractere no indice -3 para o dá $string.", PHP_EOL;
-?>
